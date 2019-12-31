11235118f41fa99d1af9898b66bf070b9b62c49a
guesshaha <931704903@qq.com>
2019-12-31 14:54:07 +0800

    Signed-off-by: guesshaha <931704903@qq.com>

--- test2/app/src/main/java/com/example/test/MainActivity.java
+++ test2/app/src/main/java/com/example/test/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button selectImageBtn = (Button)findViewById(R.id.button);
+
+        selectImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text1=(TextView) findViewById(R.id.text1);
+
+                if(text1.getText()=="Hello World!"){
+                    text1.setText("fuck");
+                }else{
+                    text1.setText("Hello World!");
+                }
+
+            }
+        });
 
     }
 }
