2378e65678a9e824e5133ae022155cee76206817
guesshaha <931704903@qq.com>
2019-12-31 14:12:27 +0800

    first
    Signed-off-by: guesshaha <931704903@qq.com>

---
+++ test2/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button selectImageBtn = (Button)findViewById(R.id.button);
+
+    }
+}
