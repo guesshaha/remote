9db196885d115e66d3ccad244716d3ba31fdc476
guesshaha <931704903@qq.com>
2020-01-06 18:20:31 +0800

    Signed-off-by: guesshaha <931704903@qq.com>

--- test2/app/src/main/java/com/example/test/MainActivity.java
+++ test2/app/src/main/java/com/example/test/MainActivity.java
@@ -7,30 +7,48 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
+import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    final int[] IMG ={R.drawable.img2} ;
-    int tempfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button selectImageBtn = (Button)findViewById(R.id.button);
 
+        VideoView videoView = (VideoView)findViewById(R.id.videoView);
+        //加载指定的视频文件
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" +R.raw.fire_video));
+        //创建MediaController对象
+        MediaController mediaController = new MediaController(this);
+
+        //VideoView与MediaController建立关联
+        videoView.setMediaController(mediaController);
+
+        //让VideoView获取焦点
+        videoView.requestFocus();
+
         staticLoadCVLibraries();
+        
 
         selectImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,10 +57,10 @@ public class MainActivity extends AppCompatActivity {
 
                 if(text1.getText().toString().equals("Hello World!")){
                     text1.setText("fuck");
-                    convernomal();
+                    convertGray();
                 }else{
                     text1.setText("Hello World!");
-                    convertGray();
+                    convernomal();
                 }
 
             }
@@ -66,17 +84,8 @@ public class MainActivity extends AppCompatActivity {
         ImageView img1=(ImageView) findViewById(R.id.imageView);
         Bitmap bmp= ((BitmapDrawable)img1.getDrawable()).getBitmap();
 
-        //1.创建一个与原图大小一致的拷贝，相当于创建了一张与原图大小一致的白纸
-        Bitmap bmCopy = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
-
-        //2.创建画笔
-        Paint paint = new Paint();
-
-        //3.创建画板，然后把白纸铺在画板上
-        Canvas canvas = new Canvas(bmCopy);
-
-        //4.开始作画
-        canvas.drawBitmap(bmp, new Matrix(), paint);
+        //创建副本
+        Bitmap bmCopy=copy_img(bmp);
 
         Utils.bitmapToMat(bmCopy, src);
         Imgproc.cvtColor(src, temp, Imgproc.COLOR_BGRA2BGR);
@@ -89,11 +98,27 @@ public class MainActivity extends AppCompatActivity {
     private void convernomal() {
 
         ImageView img1=(ImageView) findViewById(R.id.imageView);
-        tempfile=IMG[0];
-        img1.setImageDrawable(getResources().getDrawable(tempfile));
+        img1.setImageDrawable(getResources().getDrawable(R.drawable.img2));
 
     }
 
+    private Bitmap copy_img(Bitmap bmp) {
+        //1.创建一个与原图大小一致的拷贝，相当于创建了一张与原图大小一致的白纸
+        Bitmap bmCopy = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
+
+        //2.创建画笔
+        Paint paint = new Paint();
+
+        //3.创建画板，然后把白纸铺在画板上
+        Canvas canvas = new Canvas(bmCopy);
+
+        //4.开始作画
+        canvas.drawBitmap(bmp, new Matrix(), paint);
+
+        return bmCopy;
+
+
+    }
 
 
 }
